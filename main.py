18f6e226f3feb529ee4c9e43b58f3fdf9953deea
argonisbabo <argonisbabo@gmail.com>
2026-02-01 18:47:49 -0500

    My first step of python : Input and sum

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello, this is my first trash program")
+
+x = int(input("숫자 하나 입력: "))
+y = int(input("숫자 하나 더 입력: "))
+
+print("합:", x + y)
+
+# I made my first step python code with Chatgpt-5.2 for github
+# 2026/2/1/ 6:46PM 미국 인디애나 시간 기준 "ROK"
